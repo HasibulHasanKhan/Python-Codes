644cd11fde8bdc405107f0efa9cd8a77d3f84c76
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-01-29 00:25:44 +0600

    Numbers

--- DataType/Task1.py
+++ DataType/Task1.py
@@ -2,6 +2,13 @@ x = 1j
 
 #display x:
 print(x)
+y = 3+5j
+
+#casting :
+x = float(1)
+y = int(2.3)
+z = 3+5j
+zz = complex(z)
 
 #display the data type of x:
-print(type(x)) 
+print(type(zz)) 
---
+++ Numbers/Task1.py
@@ -0,0 +1,5 @@
+import random
+
+num = random.randrange(1, 20)
+
+print(num)
