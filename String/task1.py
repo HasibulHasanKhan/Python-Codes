8fe4e8c89e0668f10c57e6adb731c771ab66d4a6
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-19 20:15:05 +0600

    modify

--- String/task1.py
+++ String/task1.py
@@ -22,6 +22,20 @@
 
 #--------------------------
 
-txt = "The name of our country is Bangladesh"
+# txt = "The name of our country is Bangladesh"
 
-print("is" in txt)
+# print("is" in txt)
+
+#-------------------------------------------
+
+b = "Hello, World! "
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+# print(b.upper())
+# print(b.lower())
+# # strip() removes any whitespace to the beggining or end.
+# a = " Hello, World! "
+# print(a.strip())
+print(b.replace("H","S"))
+print(b.split(","))
