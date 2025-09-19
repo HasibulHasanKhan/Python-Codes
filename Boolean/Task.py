f485cd541144587aca7fdab42e249a627ec553de
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-20 01:49:02 +0600

    .

---
+++ Boolean/Task.py
@@ -0,0 +1,16 @@
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+a = 223
+b = 433
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+print(isinstance(a, int))
---
+++ List/Task.py
@@ -0,0 +1,13 @@
+#There are 4 type of build in data types in Python used to store collections of data.
+#List are ordered , changable , and allow duplicate values.
+
+# thislist = ["apple", "banana","cherry"]
+# print(len(thislist))
+
+# # create a new list like list()
+
+# thislist = list(("apple", "banana", "cherry"))
+# print(thislist)
+
+my_set = {"a", "b", "c", "d"}
+print(my_set)
