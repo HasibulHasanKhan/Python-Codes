63304ec2b48478f188a6304731ee13289a0df7ed
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-26 00:34:57 +0600

    remove

---
+++ List/add.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.append("orange")
+print(thislist)
+
+# extend list from another list .
+thislist.extend(tropical)
+print(thislist)
---
+++ List/remove.py
@@ -0,0 +1,18 @@
+thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# remove specified space.
+
+# thislist.pop(1)
+# print(thislist)
+
+# delete list 
+# del thislist[0]
+# print(thislist)
+# del thislist
+
+## clear list
+thislist.clear()
+
+print(thislist)
