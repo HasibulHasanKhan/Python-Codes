2154ad44f6d5c2d86b51c9a6d74c60a0ca5bbf60
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-25 23:56:47 +0600

    change list items

---
+++ List/task2.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+thislist[1:3] = ["blackcurrent", "watermelon"]
+
+print(thislist)
+
+# Insert items without replacing value.
+thislist.insert(2, "Hasib")
+print(thislist)
