69f5a71a7d8588095c0ee71545c8a20fb57cd015
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-25 23:50:19 +0600

    .

---
+++ List/Task1.py
@@ -0,0 +1,7 @@
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+print(type(thislist))
+print((thislist[1:3]))
+
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
