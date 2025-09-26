4772d3fae6e05b6d1b463823994f174c80724461
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-27 00:01:59 +0600

    .

---
+++ Tuple/task.py
@@ -0,0 +1,12 @@
+# tuple are ordered | unchangeable | duplicate not allowed | access by index 
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# thistuple2 = tuple(("apple", "banana", "cherry"))
+
+# print(len(thistuple))
+# print(thistuple[1])
+
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
+
+# print(thistuple[2:5])
---
+++ Tuple/update.py
@@ -0,0 +1,17 @@
+# x = ("apple", "banana", "cherry")
+
+# y = list(x)
+# y[1] = "kiwi"
+# y.append("orange")
+# x = tuple(y)
+
+# print(x)
+
+# thistuple = ("apple", "banana", "cherry")
+# # y = ("orange",)
+# # thistuple += y
+
+
+# # del thistuple
+
+# print(thistuple)
