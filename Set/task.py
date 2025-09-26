70fa12e2fe8881830173a463197779842ee5897a
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-27 01:40:19 +0600

    items

---
+++ Set/task.py
@@ -0,0 +1,15 @@
+# A set is a collection which is unordered, unchangeable, unindexed.
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+# False4 and 0 is considered the same value.
+
+# thisset = {"apple", "banana", "cherry", False, True, 0}
+
+## set constructor .
+
+items = set(("apple", "banana", "cherry"))
+
+print(len(thisset))
+print(type(thisset))
+print(thisset)
