ae3847e5668060c1278a95eb6581d47d13d9bc58
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-28 01:26:19 +0600

    .

---
+++ Condition/task.py
@@ -0,0 +1,15 @@
+a = 44
+b = 32
+
+# In sort
+# if a > b: print("a is greater than b")
+# print("A") if a > b else print("B")
+
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("they are equal")
+# else:
+#     print("a is greater than b")
+
+# print("a") if a > b else print("=") if a==b else print("B")
