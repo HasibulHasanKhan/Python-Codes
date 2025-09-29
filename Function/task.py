a957158788f9f9e40cd7ccce2bcccf6bd9542398
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-30 00:17:47 +0600

    .

--- Function/task.py
+++ Function/task.py
@@ -25,9 +25,28 @@
 
 ##default value as a prarameter :
 
-def myFunc5(country = "Norway"):
-    print("I am from " + country)
+# def myFunc5(country = "Norway"):
+#     print("I am from " + country)
 
-myFunc5("Sweden")
-myFunc5("Bangladesh")
-myFunc5("England")
+# myFunc5("Sweden")
+# myFunc5("Bangladesh")
+# myFunc5("England")
+
+## Pasing list as a parameter :
+# def myFunc6(food):
+#     for x in food:
+#         print(x)
+
+# fruits = [1,25,5,5,21,22]
+# myFunc6(fruits)
+
+## Pass :
+# def myFunc7():
+#     pass
+
+## , / are used to allowed to positional argument.
+
+## * used for only keyword argument .
+def myFun8(*, x):
+    print(x)
+myFun8( x = 7)
