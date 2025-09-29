b4816b32232dca2ad893b2c783ee1c26306e5e14
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-09-30 00:06:37 +0600

    .

---
+++ Function/task.py
@@ -0,0 +1,26 @@
+# def myFunc(fname, lname):
+#     print(fname + " " + lname)
+
+# myFunc("Hasibul", "Hasan")
+
+## arbitary arguments :
+
+# def myFunc2(*kids):
+#     print("The youngest child is " +  kids[2])
+
+# myFunc2("Hasib", "Hasan", "Emon")
+
+## Keyword Arguments :
+# def myFunc3(child1,child2,child3):
+#     print("The youngest child is " + child2)
+
+# myFunc3(child1 = "Hasib" , child2 = "Hasan", child3 = "Emon")
+
+## Arbitary Keyword Arguments, **kwargs:
+
+# def myFunc4(**kid):
+#     print("His last name is " + kid["lname"])
+
+# myFunc4(fname = "Tobias", lname = "Refsnes")
+
+##
