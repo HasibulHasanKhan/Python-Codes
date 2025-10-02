0853d8ba1e82b84af58a1ca89455123bf85b2bf2
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-03 02:42:07 +0600

    input

---
+++ 03_user-input/task1.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+# age = int(input("How old are you?: "))
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}")
+print(f"age is {age}")
