ead1445a72c08a43357c671922ebdef3b56fa8ee
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-03 02:35:29 +0600

    typecasting

---
+++ 02_typecasting/task1.py
@@ -0,0 +1,23 @@
+ # str(), int(), float(), bool()
+
+# name = "Hasib"
+name = ""
+age = 22
+gpa = 3.23
+is_student = True
+
+# gpa = int(gpa)
+# age = float(age)
+# age = str(age)
+# age += "1" # 1 will be error.
+
+name = bool(name)
+
+# print(age)
+# print(gpa)
+print(name)
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
