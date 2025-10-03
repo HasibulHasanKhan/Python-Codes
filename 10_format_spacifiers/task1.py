b961a35f44d8a9ef4af342cab18a9f6a2a98fe78
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-03 15:33:11 +0600

    .

--- 10_format_spacifiers/task1.py
+++ 10_format_spacifiers/task1.py
@@ -1,1 +1,40 @@
   # format specifires = {value:flags} format a value based on what flags are inserted.
+
+price1 = 12.1211111
+price2 = -2000.221111111
+price3 = 27111111
+
+
+
+##
+# print(f"Price 1 is ${price1:,}")
+# print(f"Price 2 is ${price2:,}")
+# print(f"Price 3 is ${price3:,}")
+
+##
+# print(f"Price 1 is ${price1:+10}")
+# print(f"Price 2 is ${price2:+10}")
+# print(f"Price 3 is ${price3:+10}")
+
+## center 
+# print(f"Price 1 is ${price1:^10}")
+# print(f"Price 2 is ${price2:^10}")
+# print(f"Price 3 is ${price3:^10}")
+
+# print(f"Price 1 is ${price1:>10}")#right justify
+# print(f"Price 2 is ${price2:>10}")
+# print(f"Price 3 is ${price3:>10}")
+
+# print(f"Price 1 is ${price1:<10}")
+# print(f"Price 2 is ${price2:<10}")
+# print(f"Price 3 is ${price3:<10}")
+
+
+# print(f"Price 1 is ${price1:010}") ## 0 padded
+# print(f"Price 2 is ${price2:010}")
+# print(f"Price 3 is ${price3:010}")
+
+# print(f"Price 1 is ${price1:.2f}")
+# print(f"Price 2 is ${price2:.2f}")
+# print(f"Price 3 is ${price3:.2f}")
+
