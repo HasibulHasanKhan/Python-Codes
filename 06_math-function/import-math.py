0f491197d83e10feeb2ddeccd84761ff04c3ce90
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-03 12:55:37 +0600

    .

--- 06_math-function/import-math.py
+++ 06_math-function/import-math.py
@@ -1,2 +1,21 @@
 import math
 
+# x = 9.2
+
+# print(math.pi)
+# print(math.e)
+
+# result1 = math.sqrt(x)
+# result2 = math.ceil(x)
+# result3 = math.floor(x)
+
+# print(result1)
+# print(result2)
+# print(result3)
+
+##--------------------------------
+radius = float(input("Enter the radious of a circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The circumferenc is: {round(circumference)}")
