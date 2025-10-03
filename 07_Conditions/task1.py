f9ee135d23f36483f93654ccb9ff34664bb65ffe
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-03 13:35:57 +0600

    .

---
+++ 07_Conditions/task1.py
@@ -0,0 +1,10 @@
+age = 100
+
+if age >= 100:
+    print("You are too old to sign up")
+elif age >= 18:
+    print("You are now signed up!")
+elif age < 0:
+    print("You have not born yet!")
+else:
+    print("You must be 18+ to sing up")
